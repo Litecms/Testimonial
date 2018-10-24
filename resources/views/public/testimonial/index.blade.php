e43fa04cee4e1fd1fb5e8130b0255474d993ab13
George John <george@renfos.com>
2018-10-24 13:06:20 +0530

    Update view.

--- resources/views/public/testimonial/index.blade.php
+++ resources/views/public/testimonial/index.blade.php
@@ -1,13 +1,4 @@
-<section class="title">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-8 col-md-offset-2 text-center">
-        <h1>Testimonials</h1>
-        <p>We are a creative studio focusing on culture, luxury, editorial & art. Somewhere between sophistication and simplicity.</p>
-      </div>
-    </div>
-  </div>
-</section>
+@include('testimonial::testimonial.partial.header')
 
 <section class="content bg-grey">
     <div class="testimonials">
--- resources/views/public/testimonial/partial/header.blade.php
+++ resources/views/public/testimonial/partial/header.blade.php
@@ -1,7 +1,7 @@
 <section class="title">
   <div class="container">
     <div class="row">
-      <div class="col-md-8 col-md-offset-2 text-center">
+      <div class="col-md-12 col-md-offset-12 text-center">
          <h1>{!! $title ?? trans('testimonial::testimonial.title.main') !!}</h1>
          <p> {!! $subtitle ??  trans('testimonial::testimonial.title.sub') !!}</p>
       </div>
